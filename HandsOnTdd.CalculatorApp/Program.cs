82cd3e1ff5b6eccbb46cecd1fb3dd6181aa7710d
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2022-05-14 20:59:13 -0300

    Adicionado projeto do CalculatorApp

---
+++ HandsOnTdd.CalculatorApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HandsOnTdd.CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
