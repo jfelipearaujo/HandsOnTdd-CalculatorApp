4cc2d4419dd6c4641625fac896cbd2a464356a3a
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2022-05-14 23:11:37 -0300

    Adicionado testes da parte 2

--- HandsOnTdd.CalculatorApp.Tests/CalculatorAppTests.cs
+++ HandsOnTdd.CalculatorApp.Tests/CalculatorAppTests.cs
@@ -1,5 +1,7 @@
 using FluentAssertions;
 
+using System;
+
 using Xunit;
 
 namespace HandsOnTdd.CalculatorApp.Tests
@@ -121,6 +123,202 @@ namespace HandsOnTdd.CalculatorApp.Tests
 
             // Assert
             resultado.Should().Be(resultadoEsperado);
+        }
+
+        [Fact]
+        public void Nao_Deve_Somar_Varios_Numeros_Quando_Receber_Lista_Com_Apenas_Um_Item()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            var numeros = new int[]
+            {
+                1
+            };
+
+            // Act
+            Action action = () => calculadora.SomarVariosNumeros(numeros);
+
+            // Assert
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("A lista deve ter pelo menos 2 itens para serem somados");
+        }
+
+        [Theory]
+        [InlineData(new int[] { })]
+        [InlineData(null)]
+        public void Nao_Deve_Somar_Varios_Numeros_Quando_Receber_Lista_Vazia(int[] numeros)
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            Action action = () => calculadora.SomarVariosNumeros(numeros);
+
+            // Assert
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("A lista não pode estar vazia");
+        }
+
+        [Fact]
+        public void Nao_Deve_Subtrair_Varios_Numeros_Quando_Receber_Lista_Com_Apenas_Um_Item()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            var numeros = new int[]
+            {
+                1
+            };
+
+            // Act
+            Action action = () => calculadora.SubtrairVariosNumeros(numeros);
+
+            // Assert
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("A lista deve ter pelo menos 2 itens para serem subtraídos");
+        }
+
+        [Theory]
+        [InlineData(new int[] { })]
+        [InlineData(null)]
+        public void Nao_Deve_Subtrair_Varios_Numeros_Quando_Receber_Lista_Vazia(int[] numeros)
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            Action action = () => calculadora.SubtrairVariosNumeros(numeros);
+
+            // Assert
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("A lista não pode estar vazia");
+        }
+
+        [Fact]
+        public void Deve_Calcular_Valor_Do_Produto_Com_Desconto_Com_Sucesso()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            var precoProduto = 100d;
+            var valorDesconto = 0.1d;
+
+            var resultadoEsperado = 90d;
+
+            // Act
+            var resultado = calculadora.CalcularDescontoProduto(precoProduto, valorDesconto);
+
+            // Assert
+            resultado.Should().Be(resultadoEsperado);
+        }
+
+        [Fact]
+        public void Nao_Deve_Calcular_Valor_Do_Produto_Com_Desconto_Quando_Receber_Descondo_Menor_Zero()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            var precoProduto = 100d;
+            var valorDesconto = -1d;
+
+            // Act
+            Action action = () => calculadora.CalcularDescontoProduto(precoProduto, valorDesconto);
+
+            // Assert
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("O valor do desconto não pode ser menor que 0%");
+        }
+
+        [Fact]
+        public void Nao_Deve_Calcular_Valor_Do_Produto_Com_Desconto_Quando_Receber_Descondo_Maior_Um()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            var precoProduto = 100d;
+            var valorDesconto = 2d;
+
+            // Act
+            Action action = () => calculadora.CalcularDescontoProduto(precoProduto, valorDesconto);
+
+            // Assert
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("O valor do desconto não pode ser maior que 100%");
+        }
+
+        [Theory]
+        [InlineData(0d)]
+        [InlineData(-1d)]
+        public void Nao_Deve_Calcular_Valor_Do_Produto_Com_Desconto_Quando_Receber_Valor_Menor_Zero(double precoProduto)
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            var valorDesconto = 0.5d;
+
+            // Act
+            Action action = () => calculadora.CalcularDescontoProduto(precoProduto, valorDesconto);
+
+            // Assert
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("O valor do produto não pode ser menor ou igual a zero");
+        }
+
+        [Fact]
+        public void Deve_Calcular_Valor_Do_Produto_Quando_Usuario_For_Vip()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            var usuarioVip = true;
+            var precoProduto = 100d;
+            var valorDescondo = 0.1;
+
+            var resultadoEsperado = 70d;
+
+            // Act
+            var resultado = calculadora.CalcularDescontoProdutoPorUsuario(usuarioVip, precoProduto, valorDescondo);
+
+            // Assert
+            resultado.Should().Be(resultadoEsperado);
+        }
+
+        [Fact]
+        public void Deve_Calcular_Valor_Do_Produto_Quando_Usuario_Nao_Vip()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            var usuarioVip = false;
+            var precoProduto = 100d;
+            var valorDescondo = 0.1;
+
+            var resultadoEsperado = 90d;
+
+            // Act
+            var resultado = calculadora.CalcularDescontoProdutoPorUsuario(usuarioVip, precoProduto, valorDescondo);
+
+            // Assert
+            resultado.Should().Be(resultadoEsperado);
+        }
+
+        [Fact]
+        public void Nao_Deve_Calcular_Valor_Do_Produto_Quando_Usuario_Nao_Vip_Receber_Desconto_Mair_Vip()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            var usuarioVip = false;
+            var precoProduto = 100d;
+            var valorDescondo = 0.5;
+
+            // Act
+            Action action = () => calculadora.CalcularDescontoProdutoPorUsuario(usuarioVip, precoProduto, valorDescondo);
+
+            // Assert
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("O descondo para o cliente não VIP é inválido");
         }
     }
 }
--- HandsOnTdd.CalculatorApp/Calculadora.cs
+++ HandsOnTdd.CalculatorApp/Calculadora.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace HandsOnTdd.CalculatorApp
 {
@@ -26,11 +27,31 @@ namespace HandsOnTdd.CalculatorApp
 
         public int SomarVariosNumeros(int[] numeros)
         {
+            if (numeros is null || numeros.Length == 0)
+            {
+                throw new ArgumentException("A lista não pode estar vazia");
+            }
+
+            if (numeros.Length == 1)
+            {
+                throw new ArgumentException("A lista deve ter pelo menos 2 itens para serem somados");
+            }
+
             return numeros.Sum();
         }
 
         public int SubtrairVariosNumeros(int[] numeros)
         {
+            if (numeros is null || numeros.Length == 0)
+            {
+                throw new ArgumentException("A lista não pode estar vazia");
+            }
+
+            if (numeros.Length == 1)
+            {
+                throw new ArgumentException("A lista deve ter pelo menos 2 itens para serem subtraídos");
+            }
+
             var total = numeros[0];
 
             for (int i = 1; i < numeros.Length; i++)
@@ -39,6 +60,45 @@ namespace HandsOnTdd.CalculatorApp
             }
 
             return total;
+        }
+
+        public double CalcularDescontoProduto(double precoProduto, double valorDesconto)
+        {
+            if (precoProduto <= 0d)
+            {
+                throw new ArgumentException("O valor do produto não pode ser menor ou igual a zero");
+            }
+
+            if (valorDesconto < 0d)
+            {
+                throw new ArgumentException("O valor do desconto não pode ser menor que 0%");
+            }
+
+            if (valorDesconto > 1d)
+            {
+                throw new ArgumentException("O valor do desconto não pode ser maior que 100%");
+            }
+
+            var desconto = precoProduto * valorDesconto;
+
+            return precoProduto - desconto;
+        }
+
+        public double CalcularDescontoProdutoPorUsuario(bool usuarioVip, double precoProduto, double valorDesconto)
+        {
+            const double VALOR_DESCONTO_VIP = 0.3d;
+
+            if (usuarioVip)
+            {
+                return CalcularDescontoProduto(precoProduto, VALOR_DESCONTO_VIP);
+            }
+
+            if (valorDesconto > VALOR_DESCONTO_VIP)
+            {
+                throw new ArgumentException("O descondo para o cliente não VIP é inválido");
+            }
+
+            return CalcularDescontoProduto(precoProduto, valorDesconto);
         }
     }
 }
